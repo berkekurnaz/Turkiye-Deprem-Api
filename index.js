5283dfdc14eb03fea15c2a56158935131d2acba7
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2020-01-27 13:39:59 +0300

    yeniden yukleme 1

--- index.js
+++ index.js
@@ -185,6 +185,8 @@ io.on('connection', function (socket) {
     console.log('User connected');
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, function () {
+    console.log('The app is running...');
 });
